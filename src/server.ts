f98a7a174096a69f28b6bb25e39bc6d3b3c6e6e2
Jonas  Eicher <jonas.eicher.ext@pentasys.de>
2022-03-11 14:33:35 +0100

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import cors from 'cors';
+import express from 'express';
+import morgan from 'morgan';
+
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+const app = express();
+
+// Logging
+app.use(morgan('short', { stream: { write(str) { console.log(str); } } }));
+
+// handle requests with content-type: application/json and parse body as json
+app.use(express.json());
+
+app.use(cors());
+
+app.get('/info', (req, res) => {
+    res.send('hello from node');
+})
+// Register endpoints
+// routes.registerWith(app);
+
+// Start the server
+app.listen(PORT, HOST, () => {
+    console.log(`Starting server at ${HOST}:${PORT}`);
+});
