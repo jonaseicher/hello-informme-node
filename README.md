39e35df358e50096554ccfdd07c32ca3c24d68f7
Jonas Eicher <jonas.maximilian.eicher@gmail.com>
2022-08-16 17:19:14 +0200

    initial commit (cleanup)

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
+Should do roughly what medavis RIS in Dinkelsbuehl does. Listen on port 5000 for MDM and send ADT messages to localhost:10300
+
 ### Run
 
 Start dev server with nodemon (monitors for changes)
 
     npm start
-### Typescript Build
-
-Compile sources with typescript to ```target/``` folder
-
-    npm build# hello-informme-node
---
+++ src/server.js
@@ -0,0 +1,63 @@
+var hl7 = require('simple-hl7');
+
+///////////////////SERVER/////////////////////
+var app = hl7.tcp();
+
+app.use(function (req, res, next) {
+  //req.msg is the HL7 message
+  console.log('****** message received *****');
+  console.log(req.msg.log());
+  next();
+});
+
+app.use(function (req, res, next) {
+  //res.ack is the ACK
+  //acks are created automatically
+
+  //send the res.ack back
+  console.log('****** sending ack *****');
+  res.end();
+});
+
+app.use(function (err, req, res, next) {
+  //error handler
+  //standard error middleware would be
+  console.log('****** ERROR *****');
+  console.log(err);
+  var msa = res.ack.getSegment('MSA');
+  msa.setField(1, 'AR');
+  res.ack.addSegment('ERR', err.message);
+  res.end();
+});
+
+//Listen on port 7777
+app.start(5000); //optionally pass encoding here, app.start(1234, 'latin-1');
+///////////////////SERVER/////////////////////
+
+///////////////////CLIENT/////////////////////
+var client = hl7.Server.createTcpClient('localhost', 10300);
+
+//create a message
+var msg = new hl7.Message(
+  'EPIC',
+  'EPICADT',
+  'SMS',
+  '199912271408',
+  'CHARRIS',
+  ['ADT', 'A04'], //This field has 2 components
+  '1817457',
+  'D',
+  '2.5'
+);
+
+console.log('****** sending ADT message to localhost:10300 *****');
+client.send(msg, function (err, ack) {
+  if (err) {
+    console.log('****** error when sending *****');
+    console.log(err);
+    return;
+  }
+  console.log('****** ack received *****');
+  console.log(ack.log());
+});
+///////////////////CLIENT/////////////////////
