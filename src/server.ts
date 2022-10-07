0c97bc5819a8df5856e044590e9379fd9ca7a219
Vladimir Bondar <vladi.bondar@gmail.com>
2022-10-07 17:25:49 +0200

    a

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,21 @@ import cors from 'cors';
 import express from 'express';
 import morgan from 'morgan';
 
-const PORT = 3000;
+const PORT = 5555;
 const HOST = '0.0.0.0';
 
 const app = express();
 
 // Logging
-app.use(morgan('short', { stream: { write(str) { console.log(str); } } }));
+app.use(
+  morgan('short', {
+    stream: {
+      write(str) {
+        console.log(str);
+      },
+    },
+  })
+);
 
 // handle requests with content-type: application/json and parse body as json
 app.use(express.json());
@@ -16,12 +24,19 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/info', (req, res) => {
-    res.send('hello from node');
-})
+  res.send('hello from node');
+});
+
+app.get('/api/animals', (req, res) => {
+  const voices = ['moo', 'bau'];
+
+  res.json(voices);
+});
+
 // Register endpoints
 // routes.registerWith(app);
 
 // Start the server
 app.listen(PORT, HOST, () => {
-    console.log(`Starting server at ${HOST}:${PORT}`);
+  console.log(`Starting server at ${HOST}:${PORT}`);
 });
