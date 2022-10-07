845d39a1593d5c2c2e76f48c27e9b9019f6a844a
Mai Trux <maitrux@hotmail.com>
2022-10-07 17:26:11 +0200

    Sounds

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,15 @@ const HOST = '0.0.0.0';
 const app = express();
 
 // Logging
-app.use(morgan('short', { stream: { write(str) { console.log(str); } } }));
+app.use(
+  morgan('short', {
+    stream: {
+      write(str) {
+        console.log(str);
+      },
+    },
+  })
+);
 
 // handle requests with content-type: application/json and parse body as json
 app.use(express.json());
@@ -16,12 +24,12 @@ app.use(express.json());
 app.use(cors());
 
 app.get('/info', (req, res) => {
-    res.send('hello from node');
-})
+  res.send('MUAAAHAHAH RAAAUU MIAUU hihihihi');
+});
 // Register endpoints
 // routes.registerWith(app);
 
 // Start the server
 app.listen(PORT, HOST, () => {
-    console.log(`Starting server at ${HOST}:${PORT}`);
+  console.log(`Starting server at ${HOST}:${PORT}`);
 });
